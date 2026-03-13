05c29032fbe242b1099ac8d7963d3b26b713e11b
Stephen Berry <stephenberry.developer@gmail.com>
2026-03-13 10:32:47 -0500

    use file extensions for Explorer context menu

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -23,6 +23,30 @@ suite('Extension Test Suite', () => {
 		]);
 	});
 
+	test('uses file extensions for Explorer context menu visibility', () => {
+		const extension = vscode.extensions.getExtension('beve-org.beve-vscode');
+		assert.ok(extension, 'Expected BEVE extension to be installed in the test host.');
+
+		const explorerMenu = extension.packageJSON.contributes.menus['explorer/context'] as Array<{ command: string; when: string }>;
+		assert.deepStrictEqual(explorerMenu, [
+			{
+				command: 'beve.JSON_EDITOR',
+				group: 'navigation',
+				when: '!explorerResourceIsFolder && resourceExtname == .beve'
+			},
+			{
+				command: 'beve.fromJSON',
+				group: 'navigation',
+				when: '!explorerResourceIsFolder && resourceExtname == .json'
+			},
+			{
+				command: 'beve.toJSON',
+				group: 'navigation',
+				when: '!explorerResourceIsFolder && resourceExtname == .beve'
+			}
+		]);
+	});
+
 	test('Sample test', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
